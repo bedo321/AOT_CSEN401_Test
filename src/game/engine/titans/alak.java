cc01fea210d1d6a0c5bf224e61c799d902a64077
bedo321 <abdalrhman.ashry@gmail.com>
2024-02-22 19:49:38 +0200

    Don't Remove the alak class unless there's another class in that package

---
+++ src/game/engine/titans/alak.java
@@ -0,0 +1,8 @@
+package game.engine.titans;
+
+public class alak {
+    public static void main(String[] args) {
+        System.out.println("testing packages");
+        System.out.println("sure?");
+    }
+}
---
+++ src/game/gui/alak.java
@@ -0,0 +1,8 @@
+package game.gui;
+
+public class alak {
+    public static void main(String[] args) {
+        System.out.println("testing packages");
+        System.out.println("sure?");
+    }
+}
