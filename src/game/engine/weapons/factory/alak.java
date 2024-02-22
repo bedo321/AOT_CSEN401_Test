ea4ce9abeb061f4589cf7d4cfce0b2a626154446
bedo321 <abdalrhman.ashry@gmail.com>
2024-02-22 19:40:30 +0200

    Testing

--- src/game/engine/weapons/factory/alak.java
+++ src/game/engine/weapons/factory/alak.java
@@ -3,5 +3,6 @@ package game.engine.weapons.factory;
 public class alak {
     public static void main(String[] args) {
         System.out.println("testing packages");
+        System.out.println("sure?");
     }
 }
